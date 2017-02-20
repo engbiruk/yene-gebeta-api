544a05db51bd86b2546a46a9640e70981963d6a4
Biruk Tilahun <engbiruk@gmail.com>
2017-02-20 11:02:31 +0300

    Models, Routes, Controllers

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Yene-Gebeta API Project
+> Restaurant Searching, Rating and Reviewing Platform 
+
+# Introduction
+> Yene-Gebeta is a restaurant rating and review application for Ethiopians. Yene-Gebeta brings local restaurants to a system that can make them available for any users who are looking for a place to eat. It keeps track of restaurant service providers and register them on a mobile app platform to yield a better discoverability.
+
+# Objective
+> The general objective of building this platform is to provide a service that can allows peoples to search for restaurant and get their location, review and rate.
+
---
+++ app.js
@@ -0,0 +1,60 @@
+// LOAD MODULE DEPENDENCIES
+var express			    = require('express');
+var mongoose			= require('mongoose');
+var bodyparser			= require('body-parser');
+var debug			    = require('debug')('gebeta-api');
+var validator			= require('express-validator');
+
+// LOAD APP CONFIGRATION
+var config			    = require('./config');
+
+// LOAD VARIABLES
+var router			    = require('./routes');
+var authenticate		= require('./lib/authenticate');
+
+// MONGODB CONFIGURATION
+
+// connect to mongodb
+mongoose.connect(config.MONGODB_URL);
+
+// listen to mongodb connection success
+mongoose.connection.on('connected', function mongodbConnectionListener(){
+    debug('[APP] Mongodb Connected Successfuly!');
+});
+
+// listen to mongodb connection failure
+mongoose.connection.on('error', function mongodbConnectionListener() {
+    debug('[APP] Mongodb Connection Fialed!');
+
+    // try reconnecting...
+    mongoose.connect(config.MONGODB_URL);
+});
+
+// INIT APP
+var app = express();
+
+// MIDDLEWARES
+
+// set authentication middleware
+app.use(authenticate().unless({
+    // paths that are authentication is waved
+    path: ['/users/login', '/users/logout', '/users/signup']
+}));
+
+// set body parser middleware
+app.use(bodyparser.json());
+
+// set validator middleware
+app.use(validator());
+
+
+// SET ROUTER
+router(app);
+
+// LISTEN TO HTTP PORT
+app.listen(config.HTTP_PORT, function connectionListner(){
+    debug('[APP] API server running on port %s', config.HTTP_PORT);
+});
+
+// MAKE THE APP MODULE 
+module.exports = app;
